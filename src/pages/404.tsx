8305b1b009f19aa5b534b3d4cd42f726cb4766ef
Ranjit Singh <ranjit@rs-zk-macbook.local>
2020-07-27 17:45:55 +0200

    Initial project setup with TypeScript

---
+++ src/pages/404.tsx
@@ -0,0 +1,6 @@
+import { PageProps } from 'gatsby'
+import React, { FC } from 'react'
+
+const NotFoundPage: FC<PageProps> = () => <h1>Not found</h1>
+
+export default NotFoundPage
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { PageProps } from 'gatsby'
+import React, { FC } from 'react'
+
+const IndexPage: FC<PageProps> = () => <h1>Hello world!</h1>
+
+export default IndexPage
