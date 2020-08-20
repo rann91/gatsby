7d5c6861492cc4e1a367eb6c2b38c2b945d4a86a
Ranjit Singh <rann2604@gmail.com>
2020-08-20 17:12:38 +0200

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,35 @@
-import { PageProps } from 'gatsby'
-import React, { FC } from 'react'
+/* @jsx jsx */
+import { Box, Container, Heading, jsx, Text } from 'theme-ui'
+import Link from '../components/atoms/Link'
+import Section from '../components/atoms/Section'
 
-const NotFoundPage: FC<PageProps> = () => <h1>Not found</h1>
+const NotFoundPage = () => (
+  <Section>
+    <Container sx={{ textAlign: 'center' }}>
+      <Heading as="h1" variant="h1" pt={[7, null, null, 8]} pb={3}>
+        Page not found
+      </Heading>
+      <Text as="p" variant="p" pb={5}>
+        The page you have requested cannot be found.
+      </Text>
+      <Box>
+        <Link
+          link={{
+            id: '',
+            _id: '',
+            createdAt: '',
+            updatedAt: '',
+            url: '/',
+            label: 'Back to home',
+            newWindow: false
+          }}
+          sx={{
+            variant: 'buttons.primaryLarge'
+          }}
+        />
+      </Box>
+    </Container>
+  </Section>
+)
 
 export default NotFoundPage
